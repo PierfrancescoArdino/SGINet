1b94be4243951e00f23b91d008b5745319f25786
Ardino Pierfrancesco <pierfrancesco.ardino@unitn.it>
2020-12-11 15:29:02 +0100

    **first version**

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,2 @@
 from .base_model import *
-from .sp_net_model import *
-from .sg_net_model import *
-from .spg_net_model import *
 from .sgi_model import *
-from .sp_net_model_paper import *
-from .sg_net_model_paper import *
